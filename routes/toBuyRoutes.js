06aac55504e3bdfd3375eff558a414869cf497cb
three-cubed <72698356+three-cubed@users.noreply.github.com>
2021-09-28 12:48:06 +0100

    correcting accidental misedit to router.get('/buy'

--- routes/toBuyRoutes.js
+++ routes/toBuyRoutes.js
@@ -8,41 +8,14 @@ if (process.env.NODE_ENV !== 'production') {
 const paymentProcessorSecretKey = process.env.PAYMENT_PROCESSOR_SECRET_KEY;
 
 router.get('/buy', function(req, res) {
-    console.log(req.body);
-    fs.readFile('buyables.json', function(error, dataFromBuyablesFile) {
+    fs.readFile('buyables.json', function(error, data) {
         if (error) {
+            console.log('*ERROR*');
             console.log(error);
-            res.status(500).end()
         } else {
-            const buyablesJSON = JSON.parse(dataFromBuyablesFile);
-            const backendDataArray = JSONtoArray(buyablesJSON);
-            let totalToCharge = 0;
-            let receiptInfo = [];
-            req.body.items.forEach(function(itemBeingBought) {
-                const matchingBackendData = backendDataArray.find(
-                    function(backendDataItem) {
-                        return backendDataItem.id == itemBeingBought.id
-                    })
-                const subtotal = itemBeingBought.quantity * matchingBackendData.price;
-                totalToCharge += subtotal;
-                receiptInfo.push({'item': matchingBackendData.name, 'price per item': matchingBackendData.price,'quantity': itemBeingBought.quantity, 'sub-total': subtotal});
+            res.render('buy', {
+                buyablesJSON: JSON.parse(data)
             })
-            receiptInfo.push(`total: ${totalToCharge}`)
-            console.log(`total = ${totalToCharge}`);
-            if (totalToCharge <= 0) {
-                // 403 Forbidden: The request contained valid data and was understood by the server, but the server is refusing action.
-                res.status(403).json({
-                    message: `Fictional purchase aborted by server with status 403 (forbidden): total charge of ${totalToCharge} is less than or equal to zero`,
-                    receiptInfo: 'receiptInfo not applicable.',
-                    fictionalPurchaseToken: req.body.fictionalPurchaseToken
-                });
-            } else {
-                res.json({
-                    message: `Successful fictional purchase processed by server; total charge of ${totalToCharge}`,
-                    receiptInfo: receiptInfo,
-                    fictionalPurchaseToken: req.body.fictionalPurchaseToken
-                })
-            }
         }
     })
 })
